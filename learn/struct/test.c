685882887a888080defa1da4959ee1dfe81aa2dd
zhmz90 <zhmz90@gmail.com>
2016-02-26 17:24:16 +0800

    learn how to use typedef struct c *p to define new types

--- learn/struct/test.c
+++ learn/struct/test.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct A {
+  int a;
+  int b;
+};
+
+  typedef struct A *P;
+
 int main(){
   short a;
   //  printf("sizeof short is %d\n",(int)sizeof(short));
   
   char t = 67;
-  printf("t is %c\n",t);
+  //  printf("t is %c\n",t);
+  P p;
+  //a = new A
   
+
   return 0;
 }
