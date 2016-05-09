d86391b73f9f8c93be8179dfa8907f81dadbedd9
zhmz90 <zhmz90@gmail.com>
2016-05-09 08:56:46 +0800

    learn llvm and pin

---
+++ learn/c/operator.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+  int x = 10;
+  while (x --> 0) // x goes to 0
+    {
+      printf("%d ", x);
+    }
+}
---
+++ learn/c/strlen.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main(){
+  char a[] = "Hello world!";
+
+  printf("%lu\n", strlen(a));
+  return 0;
+}
+
